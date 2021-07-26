05e6a7cd48e781b38897c90f8350494ad110e44d
Rodrigo Lima <rodsl@icloud.com>
2021-07-26 13:30:21 -0300

    Updated axios post and added dotenv

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const http = require("http");
 const url = require("url");
 const querystring = require("querystring");
+const dotenv = require("dotenv").config();
 const axios = require("axios").default;
 const pixel = Buffer.from(
   "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEX///+nxBvIAAAAAXRSTlMAQObYZgAAAApJREFUeNpjYQAAAAoABUouQOkAAAAASUVORK5CYII=",
   "base64"
 );
-const urlPowerAutomate =
-  "https://prod-27.westus.logic.azure.com:443/workflows/37e64728c8f44161a3159ceb9324009e/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vFYBO9bZESS9VPQbD53qK4PU-C2ge7F6OxYsBqzXwv";
+const urlPowerAutomate = process.env.URI_POWERAUTOMATE;
 function log(type, request, query) {
   const log = {
     dateTime: new Date().toISOString(),
@@ -22,7 +22,7 @@ function log(type, request, query) {
   };
 
   console.log(JSON.stringify(log));
-  return JSON.stringify(log);
+  return log;
 }
 
 function send(response, code, description, headers, body) {
@@ -41,8 +41,10 @@ const server = http.createServer((request, response) => {
     const logData = log("click", request, query);
     axios
       .post(urlPowerAutomate, logData)
-      .then((res) => console.log(res))
-      .catch((err) => {throw new Error(err)});
+      .then((res) => console.log(res.status, res.data))
+      .catch((err) => {
+        throw new Error(err);
+      });
     return send(response, 200, "OK", { "Content-Type": "image/png" }, pixel);
   }
 
@@ -54,8 +56,10 @@ const server = http.createServer((request, response) => {
     const logData = log("click", request, query);
     axios
       .post(urlPowerAutomate, logData)
-      .then((res) => console.log(res))
-      .catch((err) => {throw new Error(err)});
+      .then((res) => console.log(res.status, res.data))
+      .catch((err) => {
+        throw new Error(err);
+      });
 
     return send(response, 302, "Found", { Location: query.url });
   }
